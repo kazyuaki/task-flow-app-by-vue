24c39451c32e3ab73f798dda6374ebbcd4eacd7a
kazyuaki <kazuyama195@gmail.com>
2026-07-07 19:37:00 +0900

    feat: redirect root page based on auth state

---
+++ node/app/pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const { fetchUser, isAuthenticated } = useAuth()
+
+await fetchUser({
+  force: true,
+  clearAuthenticatedOnFail: true,
+})
+
+if (isAuthenticated.value) {
+  await navigateTo('/tasks')
+} else {
+  await navigateTo('/login')
+}
+</script>
